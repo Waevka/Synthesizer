df7f926f3356bea3d651f096c6e1cffafcc7e9dd
OutBast <bartlomiej.wardzinski@gmail.com>
2017-11-09 21:15:35 +0100

    + UI for noise generator

---
+++ SoundGenerator/Assets/Scripts/NoiseUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseUpdater : MonoBehaviour
+{
+    [SerializeField]
+    private GenerateSound soundGenerator;
+
+    public void OnClickWhiteNoiseButton()
+    {
+
+    }
+
+    public void OnClickPinkNoiseButton()
+    {
+
+    }
+
+    public void OnClickSquareButton()
+    {
+        UpdateWave(GenerateSound.WaveType.SQUARE);
+    }
+
+    public void UpdateWave(GenerateSound.WaveType wavetype)
+    {
+        soundGenerator.SetWaveType(wavetype);
+    }
+}
