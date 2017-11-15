523d17592f132c85cbb151c74539eb7aaf3df89d
Waevka <bogna.dudaczyk@ericsson.com>
2017-11-15 22:13:50 +0100

    Refactor DSP structure, prepare debug.

--- SoundGenerator/Assets/Scripts/LowPassFilter.cs
+++ SoundGenerator/Assets/Scripts/LowPassFilter.cs
@@ -5,7 +5,7 @@ using FMOD;
 using UnityEngine;
 
 public class LowPassFilter : MonoBehaviour {
-    public int CutOffFrequency { get; set; }
+    public float CutOffFrequency { get; set; }
     public float QValue { get; set; }
     FMOD.DSP lowPassFilterDSP;
     FMOD.DSP_DESCRIPTION description;
@@ -13,48 +13,53 @@ public class LowPassFilter : MonoBehaviour {
     private GenerateSound musicCube;
     [SerializeField]
     float[] buffer = new float[1];
-
-     //probki
     [SerializeField]
-    double xn, yn, xn1, xn2, yn1, yn2;
+    float currentSampleRate;
+    [SerializeField]
+    float singleSampleRange; //jaki zakres Hz ma w sobie jedna prowka
+
+    //probki
+    [SerializeField]
+    bool coefficientsInitialized = false;
+    float[] xn, yn, xn1, xn2, yn1, yn2;
     float s, c, alpha, r, a0, a1, a2, b1, b2;
+    [SerializeField]
+    float totalMax, totalMin;
+    [SerializeField]
+    float totalMaxBuff, totalMinBuff;
 
     // Use this for initialization
     void Start() {
         CutOffFrequency = 1000;
-        xn = 0;
-        yn = 0;
-        xn1 = 0;
-        xn2 = 0;
-        yn1 = 0;
-        yn2 = 0;
         QValue = 0.5f;
 
         description = new FMOD.DSP_DESCRIPTION();
         description.read = myDSPCallback;
         FMODUnity.RuntimeManager.LowlevelSystem.createDSP(ref description, out lowPassFilterDSP);
-        musicCube.getChannelGroup().addDSP(FMOD.CHANNELCONTROL_DSP_INDEX.HEAD, lowPassFilterDSP);
+        musicCube.getChannelGroup().addDSP(FMOD.CHANNELCONTROL_DSP_INDEX.TAIL, lowPassFilterDSP);
         SetIsFilterActive(false);
     }
 
-    public double processSample(double sample, int frequency)
-    {
-        //przesuwamy poprzednie probki o 1 do tylu
-        xn2 = xn1;
-        xn1 = xn;
+    public float processSample(float sample, int sampleIndex, int channelIndex)
+    {   
+        float sampleFrequencyRange = sampleIndex * singleSampleRange;
 
-        yn2 = yn1;
-        yn1 = yn;
+        //przesuwamy poprzednie probki o 1 do tylu
+        xn2[channelIndex] = xn1[channelIndex];
+        xn1[channelIndex] = xn[channelIndex];
+
+        yn2[channelIndex] = yn1[channelIndex];
+        yn1[channelIndex] = yn[channelIndex];
 
         //obecne probki
-        xn = sample;
+        xn[channelIndex] = sample;
 
-        if (frequency > CutOffFrequency || xn2 == 0)
+        if (channelIndex == 1)
         {
-            s = Mathf.Sin((float)(2 * Mathf.PI * CutOffFrequency) / (float)(frequency));
-            c = Mathf.Cos((float)(2 * Mathf.PI * CutOffFrequency) / (float)(frequency));
-            alpha = s / (float)(2 * QValue);
-            r = (float)(1 / (1 + alpha));
+            s = Mathf.Sin((2 * Mathf.PI * CutOffFrequency) / (sampleFrequencyRange));
+            c = Mathf.Cos((2 * Mathf.PI * CutOffFrequency) / (sampleFrequencyRange));
+            alpha = s / (2 * QValue);
+            r = (1 / (1 + alpha));
 
             a0 = 0.5f * (1 - c) * r;
             a1 = (1 - c) * r;
@@ -62,14 +67,20 @@ public class LowPassFilter : MonoBehaviour {
             b1 = -2 * c * r;
             b2 = (1 - alpha) * r;
 
-            yn = (a0 * xn) + (a1 * xn1) + (a2 * xn2) - (b1 * yn1) - (b2 * yn2);
+            yn[channelIndex] = (a0 * xn[channelIndex]) + (a1 * xn1[channelIndex]) + (a2 * xn2[channelIndex])
+                - (b1 * yn1[channelIndex]) - (b2 * yn2[channelIndex]);
+            yn[channelIndex] = Mathf.Clamp(yn[channelIndex] / 32767.0f, -1.0f, 1.0f);
+            if (yn[channelIndex] <= totalMin) totalMin = yn[channelIndex];
+            if (yn[channelIndex] >= totalMax) totalMax = yn[channelIndex];
+            //yn[channelIndex] *= 32767.0f;
+            yn[channelIndex] = sample * 0.2f;
 
         } else
         {
-            yn = sample;
+            yn[channelIndex] = sample;
         }
 
-        return yn;
+        return yn[channelIndex];
     }
 
     // Update is called once per frame
@@ -87,11 +98,25 @@ public class LowPassFilter : MonoBehaviour {
     public void SetIsFilterActive(bool enabled)
     {
         lowPassFilterDSP.setBypass(!enabled);
+        if (enabled)
+        {
+            Channel c;
+            musicCube.getChannelGroup().getChannel(0, out c);
+            c.getFrequency(out currentSampleRate);
+            coefficientsInitialized = false;
+        }
     }
 
     private RESULT myDSPCallback(ref DSP_STATE dsp_state, IntPtr inbuffer, IntPtr outbuffer, uint length, int inchannels, ref int outchannels)
     {
         buffer = new float[length * inchannels];
+        singleSampleRange = ((currentSampleRate / 2) / length); // nyquist
+        float currentSample;
+
+        if (!coefficientsInitialized)
+        {
+            initCoefficients(inchannels);
+        }
 
         int i = 0;
         try
@@ -101,10 +126,13 @@ public class LowPassFilter : MonoBehaviour {
             
                 //test dostepu pamieci
                 var currentPtr = new IntPtr(inbuffer.ToInt32() + (i * sizeof(float)));
-                buffer[i] = (float)System.Runtime.InteropServices.Marshal.PtrToStructure(
+                currentSample = (float)System.Runtime.InteropServices.Marshal.PtrToStructure(
                 currentPtr, typeof(float));
-                buffer[i] *= 0.5f; // test - zmniejszamy głośność
+                float bt = processSample(currentSample, (i/inchannels), (i%inchannels));
+                buffer[i] = bt; // test - zmniejszamy głośność
 
+                if (currentSample <= totalMinBuff) totalMinBuff = currentSample;
+                if (currentSample >= totalMaxBuff) totalMaxBuff = currentSample;
             }
 
             //kopiujemy caly bufor do wskaznika data
@@ -134,5 +162,17 @@ public class LowPassFilter : MonoBehaviour {
         }*/
 
         return RESULT.OK;
+    }
+
+    private void initCoefficients(int inchannels)
+    {
+        //inicjalizujemy tablice
+        xn = new float[inchannels];
+        yn = new float[inchannels];
+        xn1 = new float[inchannels];
+        xn2 = new float[inchannels];
+        yn1 = new float[inchannels];
+        yn2 = new float[inchannels];
+        coefficientsInitialized = true;
     }
 }
