faf593560ff2852e7ab8be347ba92302a82bede7
OutBast <bartlomiej.wardzinski@gmail.com>
2017-11-19 12:21:29 +0100

    + UI for Envelope Generator

---
+++ SoundGenerator/Assets/Scripts/EnvelopeGeneratorUpdater.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvelopeGeneratorUpdater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void IsEnabled(bool enable)
+    {
+
+    }
+
+    public void AValueUpdate(float v)
+    {
+
+    }
+
+    public void DValueUpdate(float v)
+    {
+
+    }
+
+    public void SValueUpdate(float v)
+    {
+
+    }
+
+    public void RValueUpdate(float v)
+    {
+
+    }
+}
