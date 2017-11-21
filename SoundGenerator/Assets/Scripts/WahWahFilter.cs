10e1d132b1e0319c8e68a800833dc1d05d87a2f0
Waevka <waveswallow@gmail.com>
2017-11-21 01:45:42 +0100

    Added WahWah filter.

---
+++ SoundGenerator/Assets/Scripts/WahWahFilter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WahWahFilter : FilterBase
+{
+    public float CutOffFrequency;
+    public float FrequencyRange;
+    public float Frequency;
+    [SerializeField]
+    private GenerateSound musicCube;
+    [SerializeField]
+    float totalMax, totalMin;
+    public float QValue { get; set; }
+    public int totalSamples = 0;
+    double position;
+    public float CurrentSampleFrequency;
+
+     //probki
+    [SerializeField]
+    bool coefficientsInitialized = false;
+    float[] xn, yn, xn1, xn2, yn1, yn2;
+    float s, c, alpha, r, a0, a1, a2, b1, b2;
+
+    public override float ProcessSample(float sample, int sampleIndex, int channelIndex, float currentSampleRate, int totalChannels)
+    {
+        if (!coefficientsInitialized)
+        {
+            initCoefficients(totalChannels);
+        }
+
+        position = Frequency * totalSamples / currentSampleRate;
+        totalSamples++;
+        CurrentSampleFrequency = CutOffFrequency + FrequencyRange * ((float)GenerateSineSample(position - Math.Floor(position)));
+
+        //przesuwamy poprzednie probki o 1 do tylu
+        xn2[channelIndex] = xn1[channelIndex];
+        xn1[channelIndex] = xn[channelIndex];
+
+        yn2[channelIndex] = yn1[channelIndex];
+        yn1[channelIndex] = yn[channelIndex];
+
+        //obecne probki
+        xn[channelIndex] = sample;
+
+        s = Mathf.Sin((2 * Mathf.PI * CurrentSampleFrequency) / (currentSampleRate));
+        c = Mathf.Cos((2 * Mathf.PI * CurrentSampleFrequency) / (currentSampleRate));
+        alpha = s / (2 * QValue);
+        r = (1 / (1 + alpha));
+
+        a0 = 0.5f * (1 - c) * r;
+        a1 = (1 - c) * r;
+        a2 = a0;
+        b1 = -2 * c * r;
+        b2 = (1 - alpha) * r;
+
+        yn[channelIndex] = (a0 * xn[channelIndex]) + (a1 * xn1[channelIndex]) + (a2 * xn2[channelIndex])
+            - (b1 * yn1[channelIndex]) - (b2 * yn2[channelIndex]);
+        yn[channelIndex] = Mathf.Clamp(yn[channelIndex], -1.0f, 1.0f);
+        if (yn[channelIndex] <= totalMin) totalMin = yn[channelIndex];
+        if (yn[channelIndex] >= totalMax) totalMax = yn[channelIndex];
+        //yn[channelIndex] *= 32767.0f;
+        //yn[channelIndex] = sample * 0.2f;
+        return yn[channelIndex];
+    }
+
+    private void Awake()
+    {
+        filterIndex = 3;
+        CutOffFrequency = 300.0f;
+        FrequencyRange = 600.0f;
+        Frequency = 1.0f;
+        QValue = 2.0f;
+    }
+
+    // Use this for initialization
+    void Start () {
+        SetIsFilterActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private double GenerateSineSample(double position)
+    {
+        return (Math.Sin(position * Math.PI * 2) + 1.0f) / 2.0f;
+    }
+
+    public override void SetIsFilterActive(bool enabled)
+    {
+        base.SetIsFilterActive(enabled);
+        if (!enabled)
+        {
+            coefficientsInitialized = false;
+        }
+    }
+
+    private void initCoefficients(int inchannels)
+    {
+        //inicjalizujemy tablice
+        xn = new float[inchannels];
+        yn = new float[inchannels];
+        xn1 = new float[inchannels];
+        xn2 = new float[inchannels];
+        yn1 = new float[inchannels];
+        yn2 = new float[inchannels];
+        coefficientsInitialized = true;
+    }
+}
---
+++ SoundGenerator/Assets/Scripts/WahWahFilterUpdater.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WahWahFilterUpdater : MonoBehaviour {
+    [SerializeField]
+    WahWahFilter wahWahFilter;
+    [SerializeField]
+    Text FreqRangeText;
+    [SerializeField]
+    Text FreqText;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void WahWahBaseFreqUpdater(string s)
+    {
+        Debug.Log(s);
+        int freqToSet;
+        if (!Int32.TryParse(s, out freqToSet))
+        {
+            freqToSet = 300;
+        }
+        if (freqToSet <= 0)
+        {
+            freqToSet = 1;
+        }
+        else if (freqToSet > 20000)
+        {
+            freqToSet = 20000;
+        }
+        wahWahFilter.CutOffFrequency = freqToSet;
+    }
+    public void WahWahFreqRangeUpdater(string s)
+    {   
+        int freqToSet;
+        if (!Int32.TryParse(s, out freqToSet))
+        {
+            freqToSet = 300;
+        }
+        if (freqToSet <= 0)
+        {
+            freqToSet = 1;
+            FreqRangeText.text = s;
+        }
+        else if (freqToSet > 20000 ||
+            wahWahFilter.CutOffFrequency + freqToSet >= 20000)
+        {
+            freqToSet = 19999 - (int)wahWahFilter.CutOffFrequency;
+            FreqRangeText.text = freqToSet.ToString();
+        }
+        Debug.Log(freqToSet);
+        wahWahFilter.FrequencyRange = freqToSet;
+    }
+
+    public void WahWahIsEnabledUpdater(bool b)
+    {
+        Debug.Log("Updating WahWah filter state, is enabled: " + b);
+        wahWahFilter.SetIsFilterActive(b);
+    }
+    public void FrequencyValueUpdater(float q)
+    {
+        FreqText.text = q.ToString();
+        wahWahFilter.Frequency = q;
+    }
+}
